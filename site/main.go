604e988d90ded6ef553f76c81bedda8286209696
Graham Steffaniak <graham.steffaniak@autodesk.com>
2024-05-20 13:35:24 -0500

    updated readme

--- site/main.go
+++ site/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gtsteffaniak/hi-time-live/routes"
@@ -12,6 +13,7 @@ func main() {
 	routes.SetupRoutes(e)
 	// attempt tls first, fallback to unsecure
 	if err := e.StartTLS(":9012", "cert.pem", "key.pem"); err != http.ErrServerClosed {
+		log.Println("WARNING: The server is running with HTTP instead of HTTPS, please ensure its accessed behind a reverse proxy with HTTPS.")
 		e.Logger.Fatal(e.Start(":9012"))
 	}
 }
