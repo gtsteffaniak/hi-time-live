a7423e7a1e6e43565e315e172029694c79bc17a2
Graham Steffaniak <42989099+gtsteffaniak@users.noreply.github.com>
2024-06-01 10:04:25 -0500

    Update templates logging (#11)
    * updated with grid style
    
    * updated styling border
    
    * fixed aspect ratio and bugs
    
    * fixed support link
    
    * updated

---
+++ site/templates/js/room.js
@@ -0,0 +1,18 @@
+function isSafari() {
+    var ua = navigator.userAgent.toLowerCase();
+    return ua.indexOf('safari') != -1 && ua.indexOf('chrome') == -1 && ua.indexOf('android') == -1;
+}
+
+function safariContinue() {
+    const privacyModal = document.getElementById('privacyModal');
+    privacyModal.classList.remove("hidden")
+    let modal = document.getElementById('safariModal');
+    modal.classList.add("hidden")
+}
+
+if (isSafari()) {
+    let modal = document.getElementById('safariModal');
+    modal.classList.remove("hidden")
+    const privacyModal = document.getElementById('privacyModal');
+    privacyModal.classList.add("hidden")
+}
